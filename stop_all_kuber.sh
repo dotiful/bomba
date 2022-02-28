ea9ab3acf5052b7122b544bba19bc859960c8d0d
ozeron <ozeron@me.com>
2022-02-28 19:09:11 +0200

    Update docs and scripts for kubernetes

--- stop_all_kuber.sh
+++ stop_all_kuber.sh
@@ -13,8 +13,8 @@ IFS=$'\n' read -d '' -r -a lines < $RESOURCES_FILE
 for i in "${lines[@]}"
 do
    echo "$i"
-   export REPLICAS=25
    export URL=$i
+   export REPLICAS=0
    export NAME=`echo $i| sed 's/\///g'|sed 's/://g'`
    envsubst < "bombardier.yaml" > "bombardier_dst.yaml"
    kubectl apply -f bombardier_dst.yaml
