54682394f5e25acee67ee67f3114539ceb2c3f71
almerico <almerico@gmail.com>
2022-03-01 10:33:12 +0200

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,11 +1,19 @@
 terraform {
+  required_version = "~> 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = ">= 1.0"
     }
   }
+
+  backend "s3" {
+    region         = "us-east-1"
+    key            = "terraform.tfstate"
+  }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
