11c92b47aff47b90f9cc45584743ad620f7199d7
dotiful <artdevjs@gmail.com>
2022-03-04 11:31:15 +0200

    allow run run_windows.bat on double click

--- run_windows.bat
+++ run_windows.bat
@@ -1,5 +1,5 @@
 @echo off
-for /f "tokens=*" %%s in (resources_all.txt) do (
+for /f "tokens=*" %%s in (%cd%\resources_all.txt) do (
   docker run -d --rm alpine/bombardier -c 1000 -d 60000h -l %%s
 )
 pause
