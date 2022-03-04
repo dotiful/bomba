0d13a7dbc820db3bd1f467f1faf32276b2dc8775
dotiful <artdevjs@gmail.com>
2022-03-04 11:30:33 +0200

    Added a script to stop all containers

---
+++ stop_all_windows.bat
@@ -0,0 +1,7 @@
+@echo off
+set _cmd="docker ps -a -q --filter ancestor=alpine/bombardier --format={{.ID}}"
+for /f "tokens=*" %%s in ('%_cmd%') do (
+  docker stop %%s
+)
+echo All docker container stopped.
+pause
